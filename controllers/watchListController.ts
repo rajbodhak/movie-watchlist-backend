2c2e46eae69fa4a490f15828d6462c5ad7a89f31
Raj Bodhak <rajlaptop2004@gmail.com>
2026-09-02 18:00:14 +0530

    feat(watchlist): Implement and secure user watchlist retrieval

--- controllers/watchListController.ts
+++ controllers/watchListController.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
+import { db } from "../src/prisma/db";
 
-const getUserWatchlist = async (req: Request, res: Response) => {
+const getUserWatchlist = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const userId = req.user.id;
 
-}
+        const watchlist = await db.orm.public.WatchlistItem
+            .where({ userId })
+            .all();
+
+        return res.status(200).json(watchlist);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to fetch watchlist"
+        });
+    }
+};
 
 const addMovieToWatchlist = async (req: Request, res: Response) => {
 
--- routes/watchListRoutes.ts
+++ routes/watchListRoutes.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { getUserWatchlist, addMovieToWatchlist, deleteMovieFromWatchlist } from "../controllers/watchListController";
+import { authenticate } from "../middleware/authenticate";
+
 const watchListRouter = express.Router();
+
+//authenticate middleware 
+watchListRouter.use(authenticate);
 
 watchListRouter.get("/", getUserWatchlist);
 watchListRouter.post("/", addMovieToWatchlist);

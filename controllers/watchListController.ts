e5cce59433eceaffe6b78eef5b5ca86d9b85cead
Raj Bodhak <rajlaptop2004@gmail.com>
2026-09-03 20:19:46 +0530

    feat(watchlist): implement add/remove movie from watchlist

--- controllers/watchListController.ts
+++ controllers/watchListController.ts
@@ -21,12 +21,88 @@ const getUserWatchlist = async (
 };
 
 const addMovieToWatchlist = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user.id;
+        const { movieId, rating, status, notes } = req.body;
 
+        //check if movie is already present in db or not
+        const movie = await db.orm.public.Movie
+            .where({ id: movieId })
+            .all();
+
+        if (!movie) {
+            return res.status(404).json({
+                message: "Movie Not Found"
+            })
+        }
+
+        //check if movie is already present in watchlist or not
+        const existingWathclist = await db.orm.public.WatchlistItem
+            .where({
+                userId,
+                movieId
+            }).all();
+
+        if (existingWathclist.length > 0) {
+            return res.status(400).json({
+                message: "Movie is already present in Watchlist"
+            })
+        }
+
+        const watchlistItem = await db.orm.public.WatchlistItem
+            .create({
+                userId,
+                movieId,
+                status: status || "PLANNED",
+                rating,
+                notes
+            });
+
+        return res.status(201).json({
+            message: "Movie added to Watchlist Successfully",
+            data: {
+                watchlistItem
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to add movie to watchlist"
+        });
+    }
 }
 
-const deleteMovieFromWatchlist = async (req: Request, res: Response) => {
+const deleteMovieFromWatchlist = async (
+    req: Request,
+    res: Response
+) => {
+    try {
+        const userId = req.user.id;
+        const { movieId } = req.body;
 
-}
+        const watchlistItem = await db.orm.public.WatchlistItem
+            .where({ userId, movieId })
+            .all();
+
+        if (watchlistItem.length === 0) {
+            return res.status(404).json({
+                message: "Movie not found in watchlist"
+            });
+        }
+
+        await db.orm.public.WatchlistItem
+            .where({
+                id: watchlistItem[0].id
+            })
+            .delete();
+
+        return res.status(204).send();
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Failed to remove movie from watchlist"
+        });
+    }
+};
 
 export {
     getUserWatchlist,
